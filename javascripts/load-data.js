0a7e00d16568e0f690b80bc16f2e760f1f372321
AustinScott3573 <Ascott3573@gmail.com>
2015-08-19 14:04:12 -0500

    Second commit, template done

---
+++ javascripts/load-data.js
@@ -0,0 +1,14 @@
+define(function(require) {
+    var firebase = require("firebase");
+
+    var myFirebaseRef = new Firebase("https://trippinaustin.firebaseio.com/")
+
+    //Listen for when anything changes on the "trips" key
+    myFirebaseRef.child("trips").on("value", function(snapshot) {
+        var trips = snapshot.val();
+
+        console.log("trips", trips);
+
+    });
+
+});
