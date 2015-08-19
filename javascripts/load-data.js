1aa7210d94df88f112d3dc23bd6f3b9f67c95e44
AustinScott3573 <Ascott3573@gmail.com>
2015-08-19 15:46:08 -0500

    final commit for Wed

--- javascripts/load-data.js
+++ javascripts/load-data.js
@@ -1,14 +1,20 @@
-define(function(require) {
-    var firebase = require("firebase");
+define(function(require){
+  var firebase = require("firebase");
+  var templates = require("get-templates");
 
-    var myFirebaseRef = new Firebase("https://trippinaustin.firebaseio.com/")
+  var myFirebaseRef = new Firebase("https://trippinaustin.firebaseio.com");
 
-    //Listen for when anything changes on the "trips" key
-    myFirebaseRef.child("trips").on("value", function(snapshot) {
-        var trips = snapshot.val();
+  // Listen for when anything changes on the "trips" key
+  myFirebaseRef.child("trips").on("value", function(snapshot) {
+    var trips = snapshot.val();
 
-        console.log("trips", trips);
+    console.log("trips", trips);
 
-    });
+    // This will hold the complete DOM string of trips
+    var populatedTemplate = templates.tripTpl(trips);
+
+    // Insert the DOM string into the appropriate element
+    $("#list-of-trips").html(populatedTemplate);
+  });
 
 });
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,22 +1,21 @@
 requirejs.config({
-  baseUrl: '../javascripts',
+  baseUrl: "./javascripts",
   paths: {
-    'jquery': '../lib/bower_components/jquery/dist/jquery.min',
-    'firebase': '../lib/bower_components/firebase/firebase',
-    'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
-    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
-    'q': '../lib/bower_components/q/q' 
+    "jquery": "../lib/bower_components/jquery/dist/jquery.min",
+    "firebase": "../lib/bower_components/firebase/firebase",
+    "lodash": "../lib/bower_components/lodash/lodash.min",
+    "hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
+    "bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min",
+    "q": "../lib/bower_components/q/q"
   },
   shim: {
-    'bootstrap': ['jquery'],
-    'firebase': {
-      exports: 'Firebase'
-    }
+    "bootstrap": ["jquery"],
+    "firebase": {exports: "Firebase"}
   }
 });
 
-requirejs(["dependencies", "load-data"],
-  function(dependencies) {
+requirejs(["dependencies", "load-data", "save-data"], 
+  function(dependencies, loadData, saveData) {
 
-  });
-  
+  }
+);
